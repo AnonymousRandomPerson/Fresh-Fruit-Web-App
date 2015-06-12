e237f39669dd1d69e5ef80f59f05d2b44c786e1a
gportz3 <gportz3@gmail.com>
2015-06-12 15:18:28 -0400

    added java docs to Student user

--- FreshFruitWebApp/src/java/StudentUser.java
+++ FreshFruitWebApp/src/java/StudentUser.java
@@ -1,9 +1,14 @@
 public class StudentUser extends User {
 
+    
     public StudentUser(String username, String password) {
         super(username, password);
     }
     
+ /**
+ * Status of the user 
+ *
+ */
     public enum Status {
         Normal, Locked, Banned
     }
@@ -14,30 +19,56 @@ public class StudentUser extends User {
     private Status status;
     private String preferences;
     
+ /**
+ * Method that gets the date of birth 
+ * @return the DOB
+ */
     public int[] getDob() {
         return dob;
     }
     
+ /**
+ * Method that gets major
+ * @return the major
+ */
     public String getMajor() {
         return major;
     }
     
+ /**
+ * Method that gets interest
+ * @return the interests
+ */
     public String getInterest() {
         return interest;
     }
     
+    /**
+ * Method that gets the status of user 
+ * @return the status
+ */
     public Status getStatus() {
         return status;
     }
-    
+ /**
+ * Method that gets preferences
+ * @return the preferences 
+ */
     public String getPreferences() {
         return preferences;
     }
     
+/**
+ * Method that sets Dob
+ * @param dob takes in a DOB
+ */
     public void setDob(int[] dob) {
         this.dob = dob;
     }
-    
+/**
+ * Method that sets the major
+ * @param major takes in a major
+ */
     public void setMajor(String major) {
         this.major = major;
     }
