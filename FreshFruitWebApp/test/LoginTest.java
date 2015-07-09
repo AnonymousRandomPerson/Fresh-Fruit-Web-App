9324bfaf8157a2cb340fa1274e61572081d51c9c
hzheng40 <hzheng40@gatech.edu>
2015-07-09 11:23:52 -0400

    added LoginTest

---
+++ FreshFruitWebApp/test/LoginTest.java
@@ -0,0 +1,15 @@
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+/**
+ * JUnit Test for login()
+ * @author Hongrui Zheng
+ */
+public class LoginTest {
+    
+}
