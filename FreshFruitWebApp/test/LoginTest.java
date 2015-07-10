a3b3a27532b56aff1b5405f456c98c0fbafcc5a9
hzheng40 <hzheng40@gatech.edu>
2015-07-10 09:10:01 -0400

    added login test

--- FreshFruitWebApp/test/LoginTest.java
+++ FreshFruitWebApp/test/LoginTest.java
@@ -1,15 +1,111 @@
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import javax.faces.context.FacesContext;
 /**
  * JUnit Test for login()
  * @author Hongrui Zheng
  */
 public class LoginTest {
+    public LoginTest() {
+    }
     
+    @BeforeClass
+    public static void setUpClass() { 
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+    /**
+     * Test of login method
+     */
+    @Test
+    public void testLoginNull() {
+        UserManager um = new UserManager();
+        ProfileUI ui = new ProfileUI();
+        ui.setUserManager(um);
+        ui.setUsername(null);
+        ui.setPassword(null);
+        um.setUser(null);
+        assertNull(ui.login());
+        assertEquals("Username or password incorrect.", FacesContext.getCurrentInstance().getMessages().next().getDetail());
+    }
+    
+    @Test 
+    public void testLoginEmpty() {
+        UserManager um = new UserManager();
+        ProfileUI ui = new ProfileUI();
+        ui.setUserManager(um);
+        ui.setUsername("");
+        ui.setPassword(null);
+        um.setUser(null);
+        assertNull(ui.login());
+        assertEquals("No username entered.", FacesContext.getCurrentInstance().getMessages().next().getDetail());
+    }
+    
+    @Test
+    public void testLoginLocked() {
+        UserManager um = new UserManager();
+        ProfileUI ui = new ProfileUI();
+        StudentUser user = new StudentUser("user", "pass");
+        user.setStatus(User.Status.Locked);
+        um.setUser(user);
+        ui.setUsername("user");
+        ui.setPassword("pass");
+        assertNull(ui.login());
+        assertEquals("You have exceeded your number of attempts to log in.", FacesContext.getCurrentInstance().getMessages().next().getDetail());
+    }
+    
+    @Test
+    public void testLoginBanned() {
+        UserManager um = new UserManager();
+        ProfileUI ui = new ProfileUI();
+        StudentUser user = new StudentUser("user", "pass");
+        user.setStatus(User.Status.Banned);
+        um.setUser(user);
+        ui.setUsername("user");
+        ui.setPassword("pass");
+        assertNull(ui.login());
+        assertEquals("You have been banned from this application.", FacesContext.getCurrentInstance().getMessages().next().getDetail());
+    }
+    
+    @Test
+    public void testLoginSuccess() {
+        UserManager um = new UserManager();
+        ProfileUI ui = new ProfileUI();
+        StudentUser user = new StudentUser("user", "pass");
+        user.setStatus(User.Status.Normal);
+        um.setUser(user);
+        ui.setUsername("user");
+        ui.setUsername("pass");
+        assertEquals("home", ui.login());
+        assertEquals(true, user.checkLogin("pass"));
+    }
+    
+    @Test
+    public void testLoginFail() {
+        UserManager um = new UserManager();
+        ProfileUI ui = new ProfileUI();
+        StudentUser user = new StudentUser("user", "Wrongpass");
+        user.setStatus(User.Status.Normal);
+        um.setUser(user);
+        ui.setUsername("user");
+        ui.setUsername("pass");
+        assertNull(ui.login());
+        assertEquals(false, user.checkLogin("pass"));
+    }
 }
+    
